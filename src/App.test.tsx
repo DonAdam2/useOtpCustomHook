45da1717f0f6075d21374408173451c2c4e34417
Adam Morsi <33934746+DonAdam2@users.noreply.github.com>
2022-03-21 10:45:53 +0200

    Initial commit

---
+++ src/App.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen, cleanup } from '@testing-library/react';
+//component
+import App from './App';
+
+afterEach(cleanup);
+
+describe('App Component', () => {
+  test('renders webpack react boilerplate', () => {
+    render(<App />);
+    const title = screen.getByText(/webpack react boilerplate/i);
+    expect(title).toBeInTheDocument();
+  });
+});
---
+++ src/ts/containers/testComponent/TestComponent.test.tsx
@@ -0,0 +1,45 @@
+import { Store } from 'redux';
+// react testing library
+// import { cleanup, render, screen, fireEvent } from '@testing-library/react';
+// all providers mock
+import { cleanup, render, screen, fireEvent } from '@/jest/mock/OverrideRenderOfRTL';
+// snapshots renderer
+import renderer from 'react-test-renderer';
+// mock store provider
+import MockProvider from '@/jest/mock/MockProvider';
+// mock store
+import createMockStore from '@/jest/mock/store/createMockStore';
+//components
+import TestComponent from './TestComponent';
+
+afterEach(cleanup);
+
+describe('testComponent.jsx', () => {
+  it('snapshot renders correctly, truthy values', () => {
+    const tree = renderer
+      .create(
+        <MockProvider>
+          <TestComponent />
+        </MockProvider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('should dispatch TEST_ACTION action when user clicks the button', () => {
+    const store: Store = createMockStore({
+      app: {
+        testString: 'Initial test',
+      },
+    });
+    // using RTL render
+    /*render(
+			<MockProvider mockStore={store}>
+				<TestComponent />
+			</MockProvider>
+		);*/
+    //using the custom render with all providers
+    render(<TestComponent />, { mockStore: store });
+    fireEvent.click(screen.getByTestId('changeText'));
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+  });
+});
